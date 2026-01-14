a1a35531383cbc1416abcbdc08c576710708c86e
Kelvi Padilla <kurtkelvisk88@gmail.com>
2026-01-13 23:23:55 -0600

    Fix base path

--- login/frontend/vite.config.ts
+++ login/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/excercise_1/',
+    base: '/',
     server: {
         port: 5173,
         proxy: {
