210f463c177e8b99bba8fb616add36b207f24ed8
Kelvi Padilla <kurtkelvisk88@gmail.com>
2026-01-13 23:12:08 -0600

    Fix base path to match repository name

--- login/frontend/vite.config.ts
+++ login/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/prueba-tecnica/',  // Cambia esto por el nombre de tu repositorio en GitHub
+    base: '/excercise_1/',
     server: {
         port: 5173,
         proxy: {
